4ed47ec267f11357fdae75518df91f21b0f2b4e8
MafeLmn <maria.fdiaz11@gmail.com>
2018-10-13 22:58:33 -0500

    no message

---
+++ MaríaFernandaDíaz-TableTennis/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+    }
+    if (collision.relativeVelocity.magnitude > 2)
+        audioSource.Play();
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	}
+}
